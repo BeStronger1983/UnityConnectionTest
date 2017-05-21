46858001060200cd1673b1c48b2b5277e70ec848
ponpon <wizardsoloist1983@gmail.com>
2017-05-21 15:21:01 +0800

    add HttpRequestTest

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(DoRequest());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator DoRequest()
+	{
+		var request = UnityWebRequest.Get("https://raw.githubusercontent.com/BeStronger1983/UnityConnectionTest/master/http_get_test");
+
+		yield return request.Send();
+
+		if (request.isError)
+		{
+			Debug.Log(request.error);
+			yield break;
+		}
+
+		var html = request.downloadHandler.text;
+		Debug.Log(html);
+	}
+}
